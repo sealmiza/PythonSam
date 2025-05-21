7636a1f6f5161c1a6403ac4be21b228d5f71dc7c
sealmiza <samuelachimba90@gmail.com>
2025-05-21 17:10:58 +0300

    redone1

--- variables/name.py
+++ variables/name.py
@@ -14,7 +14,7 @@ def my_function():
 
 my_function()
 def my_function(fname):
-    print(fname+'hello')
+    print(fname + 'hello')
 
 my_function('samuel')
 my_function('esther')
@@ -40,6 +40,7 @@ def my_function(food):
     for x in food:
         print(x)
 fruits=['kiwi','grapes','watermelon']
+fruits.append('mangoes')
 
 my_function(fruits)
 x= lambda a: a*5
@@ -55,5 +56,35 @@ mytrippler=my_function(3)
 
 print(mydoubler(5))
 print(mytrippler(2))
+
+compliment=input('do you like your classes:yes|no')
+compliment=str(compliment)
+if compliment=='yes':
+    print('great, youre invited to our next lecture')
+else:
+    print('what seems to be the problem')
+
+def display_messsage(name):
+    print('hello', name ,' we are learning about functions and how to apply them')
+
+display_messsage('sammy')
+display_messsage(' clinton')
+
+def fav_book(book):
+    '''write how you feel about your fav book'''
+    print('one of my fav books is' ,book)
+
+fav_book('alice in wonderland')
+fav_book('desire of ages')
+def make_shirt(size,text):
+    print(size ,text , " is the description of shirts i like")
+
+make_shirt(31,"champion")
+make_shirt(23, "amiri")
+def describe_city(name ,country):
+    print(name,country , " is the location of the club")
+
+describe_city("nairobi" ,"kenya" )
+describe_city("kigali" ,"rwanda" )
 
 
